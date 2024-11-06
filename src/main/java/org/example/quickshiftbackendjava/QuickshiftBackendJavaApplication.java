e43771e60d6c108a1479658db88a59be411c336a
Electimore <electimore@gmail.com>
2024-11-06 14:00:53 +0100

    Initial commit

---
+++ src/main/java/org/example/quickshiftbackendjava/QuickshiftBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.quickshiftbackendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickshiftBackendJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickshiftBackendJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/quickshiftbackendjava/QuickshiftBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.quickshiftbackendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickshiftBackendJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
